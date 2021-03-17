f95cd216d4108870f6a75a381106911e0452ad7e
Nikolas <madara.alpha@gmail.com>
2021-03-17 19:03:50 -0300

    componente hecho

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 new Vue({
-                    el:'#app', components:{
+                    el:'#app',
+                    components:{
 
-                    }
+                    },
                     vuetify: new Vuetify(),
                     })
