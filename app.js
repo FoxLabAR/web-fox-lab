29d00ad1075ead383d2f0d59efa0940b1cbb90e7
madara8854 <kobouharriet@gmail.com>
2021-03-17 22:55:09 -0300

    Add components nav & home

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
+let AppBar = {
+    template: ``
+}
+
+let Home = {
+    template: ``
+}
+
+
+
 new Vue({
-                    el:'#app',
-                    vuetify: new Vuetify(),
-                    })
+        el:'#app',
+        vuetify: new Vuetify(),
+        components: {
+            'app_bar': AppBar,
+            'home': Home,
+        }
+})
